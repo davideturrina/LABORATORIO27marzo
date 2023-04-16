3ab94c18a472f720f14d0251ccbfd2d345d795c0
davideturrina <davide.turrina@live.it>
2023-04-16 18:56:32 +0200

    modifiche sontuose

--- asdasdadd.m
+++
@@ -1,4 +0,0 @@
-clear all
-
-imdata = imread('C:\Users\davide\Desktop\misure meccaniche e  termiche\laboratorio\laboratorio27marzo\dati lab\EncDataLab_ANT_LE_50_BUCO1_1.csv');
-figure(1), imagesc(imdata);
